b80e7df855a43baa64d81a745f8da1a44ca6c519
Joé Dupuis <joe@dupuis.io>
2024-10-29 18:37:15 -0700

    Remove scaffold comments

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,25 +1,20 @@
 class UploadsController < ApplicationController
   before_action :set_upload, only: %i[ show edit update destroy ]
 
-  # GET /uploads or /uploads.json
   def index
     @uploads = Upload.all
   end
 
-  # GET /uploads/1 or /uploads/1.json
   def show
   end
 
-  # GET /uploads/new
   def new
     @upload = Upload.new
   end
 
-  # GET /uploads/1/edit
   def edit
   end
 
-  # POST /uploads or /uploads.json
   def create
     @upload = Upload.new(upload_params)
 
@@ -34,7 +29,6 @@ class UploadsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /uploads/1 or /uploads/1.json
   def update
     respond_to do |format|
       if @upload.update(upload_params)
@@ -47,7 +41,6 @@ class UploadsController < ApplicationController
     end
   end
 
-  # DELETE /uploads/1 or /uploads/1.json
   def destroy
     @upload.destroy!
 
@@ -58,12 +51,10 @@ class UploadsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_upload
       @upload = Upload.find(params.expect(:id))
     end
 
-    # Only allow a list of trusted parameters through.
     def upload_params
       params.fetch(:upload, {})
     end
