3d355ae1191f36bc4ef043a771247ddd7a779f67
Joé Dupuis <joe@dupuis.io>
2024-10-29 18:13:08 -0700

    Uploads

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2024_10_30_011241) do
+ActiveRecord::Schema[8.1].define(version: 2024_10_30_011255) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,6 +46,11 @@ ActiveRecord::Schema[8.1].define(version: 2024_10_30_011241) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "uploads", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
